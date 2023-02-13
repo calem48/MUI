6ec053b028147c8b01b1b271425e9912d59f8c9f
calem48 <messaoud.messaoud92@gmail.com>
2023-02-14 00:19:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Container from "./Container"
+import Box from "@mui/material/Box"
+
+
+function App() {
+
+  return (
+    <>
+      <Container sx={{ backrgoundColor: 'blue' }} >
+        <Box>1</Box>
+        <Box>2</Box>
+      </Container>
+    </>
+  )
+}
+
+export default App
---
+++ src/Container.jsx
@@ -0,0 +1,42 @@
+import { Container } from "@mui/system";
+import { createTheme, styled, ThemeProvider } from "@mui/material";
+
+
+
+export default function CustomContainer({ children }) {
+
+    const NavbarContainer = styled(Container)(({ theme }) => ({
+        margin: "0 auto",
+        paddingLeft: "15px",
+        paddingRight: "15px",
+
+        [theme.breakpoints.up('md')]: {
+            width: "750px"
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: "970px"
+        },
+        [theme.breakpoints.up('xl')]: {
+            width: "1170px"
+        },
+    }));
+
+    const theme = createTheme({
+        breakpoints: {
+            values: {
+                sm: 576,
+                md: 768,
+                lg: 992,
+                xl: 1200,
+            },
+        },
+    });
+
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <NavbarContainer>{children}</NavbarContainer>
+        </ThemeProvider>
+    )
+}
